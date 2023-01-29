d82f96198b81345844a0ef2c5f54118b2b5380fe
ivushkin <ebazaeba@gmail.com>
2023-01-29 15:02:47 +0300

    ремонт коммита

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+<<<<<<< HEAD
 from first.nums import plus
 if __name__ == '__main__':
     print(plus(3,3))
+=======
+
+
+def print_hi(name: object) -> object:
+
+    print(f'Здарова, {name}')
+
+
+
+if __name__ == '__main__':
+    print_hi('Никита Юрьевич')
+
+>>>>>>> 31f5511 (ремонт коммита)
 
